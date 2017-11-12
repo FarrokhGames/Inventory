8104d441a92ffb9d6ed119b4d0195306d7cef7c9
DoktorAce <patrik.liljecrantz@gmail.com>
2017-11-13 00:06:09 +0100

    Replace Point with Unity's built in Vector2Int

--- Unity Project/Assets/Plugins/Farrokh Games/Inventory/Editor/Tests/InventoryShapeTests.cs
+++ Unity Project/Assets/Plugins/Farrokh Games/Inventory/Editor/Tests/InventoryShapeTests.cs
@@ -1,6 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
 using FarrokhGames.Shared;
-using System.Linq;
+using NUnit.Framework;
+using UnityEngine;
 
 namespace FarrokhGames.Inventory
 {
@@ -42,9 +43,9 @@ namespace FarrokhGames.Inventory
             var shape = new InventoryShape(s);
 
             Assert.That(shape.Points.Length, Is.EqualTo(3));
-            Assert.That(shape.Points.Contains(new Point(0, 0)), Is.True);
-            Assert.That(shape.Points.Contains(new Point(1, 1)), Is.True);
-            Assert.That(shape.Points.Contains(new Point(2, 0)), Is.True);
+            Assert.That(shape.Points.Contains(new Vector2Int(0, 0)), Is.True);
+            Assert.That(shape.Points.Contains(new Vector2Int(1, 1)), Is.True);
+            Assert.That(shape.Points.Contains(new Vector2Int(2, 0)), Is.True);
         }
 
         [Test]
@@ -68,23 +69,23 @@ namespace FarrokhGames.Inventory
             var shape = new InventoryShape(s);
 
             Assert.That(shape.Points.Count, Is.EqualTo(2));
-            Assert.That(shape.Contains(Point.zero), Is.True);
-            Assert.That(shape.Contains(Point.one), Is.True);
+            Assert.That(shape.Contains(Vector2Int.zero), Is.True);
+            Assert.That(shape.Contains(Vector2Int.one), Is.True);
         }
 
         [Test]
         public void Position_Changed()
         {
             var shape = new InventoryShape(1, 1);
-            shape.Position = Point.one;
-            Assert.That(shape.Position, Is.EqualTo(Point.one));
+            shape.Position = Vector2Int.one;
+            Assert.That(shape.Position, Is.EqualTo(Vector2Int.one));
         }
 
         [Test]
         public void Position_RectCalculated()
         {
             var shape = new InventoryShape(3, 2);
-            shape.Position = new Point(6, 9);
+            shape.Position = new Vector2Int(6, 9);
             Assert.That(shape.Rect.x, Is.EqualTo(6));
             Assert.That(shape.Rect.y, Is.EqualTo(9));
             Assert.That(shape.Rect.width, Is.EqualTo(3));
@@ -99,11 +100,11 @@ namespace FarrokhGames.Inventory
             s[1, 1] = true;
 
             var shape = new InventoryShape(s);
-            shape.Position = new Point(6, 9);
+            shape.Position = new Vector2Int(6, 9);
 
             Assert.That(shape.Points.Count, Is.EqualTo(2));
-            Assert.That(shape.Contains(new Point(6, 9)), Is.True);
-            Assert.That(shape.Contains(new Point(7, 10)), Is.True);
+            Assert.That(shape.Contains(new Vector2Int(6, 9)), Is.True);
+            Assert.That(shape.Contains(new Vector2Int(7, 10)), Is.True);
         }
 
         [Test]
@@ -114,14 +115,14 @@ namespace FarrokhGames.Inventory
             s1[0, 1] = true;
             s1[1, 1] = true;
             var shape1 = new InventoryShape(s1);
-            shape1.Position = new Point(0, 0);
+            shape1.Position = new Vector2Int(0, 0);
 
             var s2 = new bool[2, 2];
             s1[0, 1] = true;
             s1[1, 1] = true;
             s1[1, 0] = true;
             var shape2 = new InventoryShape(s2);
-            shape2.Position = new Point(0, 1);
+            shape2.Position = new Vector2Int(0, 1);
 
             Assert.That(shape1.Overlaps(shape2), Is.False);
             Assert.That(shape2.Overlaps(shape1), Is.False);
@@ -135,14 +136,14 @@ namespace FarrokhGames.Inventory
             s1[1, 0] = true;
             s1[2, 0] = true;
             var shape1 = new InventoryShape(s1);
-            shape1.Position = new Point(0, 0);
+            shape1.Position = new Vector2Int(0, 0);
 
             var s2 = new bool[1, 3];
             s2[0, 0] = true;
             s2[0, 1] = true;
             s2[0, 2] = true;
             var shape2 = new InventoryShape(s2);
-            shape2.Position = new Point(1, -1);
+            shape2.Position = new Vector2Int(1, -1);
 
             Assert.That(shape1.Overlaps(shape2), Is.True);
             Assert.That(shape2.Overlaps(shape1), Is.True);
