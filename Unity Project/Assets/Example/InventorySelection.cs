85218a759f78d6e23d4eae2a2b4a963a3ff93d35
Patrik Liljecrantz <patrik@lavapotion.com>
2020-02-06 21:26:53 +0100

    Add rider plugin

--- Unity Project/Assets/Example/InventorySelection.cs
+++ Unity Project/Assets/Example/InventorySelection.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace FarrokhGames.Inventory.Examples
--- Unity Project/Assets/Plugins/Farrokh Games/Inventory/InventoryController.cs
+++ Unity Project/Assets/Plugins/Farrokh Games/Inventory/InventoryController.cs
@@ -1,5 +1,4 @@
 using System;
-using FarrokhGames.Shared;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
